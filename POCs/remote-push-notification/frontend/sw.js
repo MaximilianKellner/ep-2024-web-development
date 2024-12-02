237596c2e950c1eaac77db64d77f1c05fd4e291f
Oliver Russmann <oliver.russmann@smail.th-koeln.de>
2024-12-02 12:35:42 +0100

    Remove unused code

--- POCs/remote-push-notification/frontend/sw.js
+++ POCs/remote-push-notification/frontend/sw.js
@@ -3,17 +3,6 @@ console.log("Service Worker Loaded...");
 self.addEventListener("push", (e) => {
     const data = e.data.json();
     console.log("Push Recieved...");
-
-    
-    const notificationData = data;
-    console.log('');
-    console.log('The notification data has the following parameters:');
-    Object.keys(notificationData).forEach((key) => {
-        console.log(`  ${key}: ${notificationData[key]}`);
-    });
-    console.log('');
-
-
     self.registration.showNotification(data.title, data.options);
 });
 
