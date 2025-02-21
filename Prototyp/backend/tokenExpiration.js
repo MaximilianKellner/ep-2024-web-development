9f0c26ab46548aa1bb3c62a64128a2f747d45043
Oliver Russmann <oliver.russmann@smail.th-koeln.de>
2025-02-21 14:47:12 +0100

    Fix token expiration check rate

--- Prototyp/backend/tokenExpiration.js
+++ Prototyp/backend/tokenExpiration.js
@@ -1,24 +1,27 @@
 import {pool} from './db.js';
 import {sendReminderNotification} from "./email-notification.js";
+
 export async function checkTokenExpired() {
     try {
-        const fullDay = 1000 * 10; // Hier vermutlich als Testwert
-        const threeDays = 1000 * 60 * 60 * 24 * 3;
+        const oneDay = 1000 * 60 * 60 * 24;
+        const threeDays = oneDay * 3; // Hier vermutlich als Testwert
         const now = Date.now();
 
         const customers = await pool.query('SELECT * FROM customer');
         customers.rows.filter(customer => {
 
+            console.log(customer.expiration_date);
+            console.log(now + threeDays >= customer.expiration_date);
             if (now + threeDays >= customer.expiration_date) {
                 const name = customer.name;
                 const email = customer.email;
-                const renewalLink = "test";
+                const renewalLink = `http://localhost:5000/${customer.link_token}`;
                 const expirationDate = customer.expiration_date;
                 sendReminderNotification(name, email, renewalLink, expirationDate);
                 console.log(customer.customer_name)
             }
         })
-        setTimeout(checkTokenExpired, fullDay);
+        setTimeout(checkTokenExpired, oneDay);
     } catch (error) {
         console.error(error);
     }
