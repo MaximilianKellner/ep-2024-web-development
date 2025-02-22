d0b7a4a5d50c48652869732293cee70ed27d2185
MKellner <maximilian_elias.kellner@smail.th-koeln.de>
2025-02-22 15:37:04 +0100

    feat: mobile cards logic --> Cards into html #159

--- Prototyp/frontend/scripts/optimized-download.js
+++ Prototyp/frontend/scripts/optimized-download.js
@@ -61,8 +61,25 @@ function createTableRow(fileData) {
     `;
 }
 
+function createMobileCard(fileData){
+    const { url, fileName, fileNameWithoutSuffix, blob, creationDate, hoursLeft } = fileData;
+
+    return `<div class="img-card-mobile">
+                <img src="${url}" alt="${fileNameWithoutSuffix}"/>
+                <div class="download-row">
+                    <div class="text-group">
+                        <p>${fileNameWithoutSuffix}</p>
+                        <label class="sublabel">${hoursLeft}h verbleibend - ${(blob.size / 1024).toFixed(0)} KB</label>
+                    </div>
+                    <button class="download-btn" data-url="${url}" data-filename="${fileNameWithoutSuffix}">
+                        <img src="./img/icon/download.svg" alt="download" onclick="this.parentElement.click()">
+                    </button>
+                </div>
+            </div>`
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    loadOptimizedTable();
+    loadOptimizedContent();
 
     const tbody = document.getElementById('image-table-body');
     const downloadButton = document.getElementById('download-selected-btn');
@@ -91,13 +108,14 @@ document.addEventListener('DOMContentLoaded', () => {
     downloadButton.addEventListener('click', downloadSelectedFiles);
 });
 
-function loadOptimizedTable() {
+function loadOptimizedContent() {
     const linkToken = window.location.pathname.replace("/", ""); // Entfernt das "/"
     console.log("Aktueller linkToken:", linkToken);
 
     const tbody = document.getElementById('image-table-body');
     const table = document.getElementById('image-table');
     const downloadButton = document.getElementById('download-selected-btn');
+    const mobileCardList = document.getElementById('mobile-card-list');
 
     axios.get(`http://localhost:5000/${linkToken}/optimized-images`)
         .then((response) => {
@@ -126,17 +144,42 @@ function loadOptimizedTable() {
                         const creationDate = new Date(parseInt(timestamp));
                         const hoursLeft = calculateHoursLeft(fileNames[index]);
 
-                        return createTableRow({
+                        const fileData = {
                             url,
                             fileName: fileNames[index],
                             fileNameWithoutSuffix: fileNamesWithoutSuffix[index],
                             blob,
                             creationDate,
                             hoursLeft
-                        });
+                        };
+
+                        return createTableRow(fileData);
+                    }).join('');
+
+                    const mobileContent = responses.map((response, index) => {
+                        const { data, headers } = response;
+                        const contentType = headers['content-type'];
+                        const blob = new Blob([data], { type: contentType });
+                        const url = URL.createObjectURL(blob);
+                        const uniqueSuffix = fileNames[index].split('-').pop();
+                        const timestamp = uniqueSuffix.split('_')[0];
+                        const creationDate = new Date(parseInt(timestamp));
+                        const hoursLeft = calculateHoursLeft(fileNames[index]);
+
+                        const fileData = {
+                            url,
+                            fileName: fileNames[index],
+                            fileNameWithoutSuffix: fileNamesWithoutSuffix[index],
+                            blob,
+                            creationDate,
+                            hoursLeft
+                        };
+
+                        return createMobileCard(fileData);
                     }).join('');
 
                     tbody.innerHTML = tableContent;
+                    mobileCardList.innerHTML = mobileContent;
                     table.classList.remove('hidden');
                     downloadButton.classList.remove('hidden');
                 })
