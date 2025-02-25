583807f18d9de183ab53e818ce0964680c9c831d
MKellner <maximilian_elias.kellner@smail.th-koeln.de>
2025-02-25 01:56:02 +0100

    feat: Email design improved #191

--- Prototyp/backend/NotificationMessageBuilder.js
+++ Prototyp/backend/NotificationMessageBuilder.js
@@ -5,23 +5,39 @@ class NotificationMessageBuilder {
         switch (messageType) {
             case NotificationMessageType.REMINDER: {
                 return `
-        <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f4f4f4; text-align: center;">
-            <h1 style="color: #333;">Your personal link will expire soon!</h1>
-            <p style="color: #555; font-size: 16px;">Hello ${customerName},</p>
-            <p style="color: #555; font-size: 16px;">Your personal download link will expire on <strong>${expirationDate}</strong>. Make sure to renew it before it's too late!</p>
-            <a href="${customerLink}" style="display: inline-block; padding: 12px 20px; margin-top: 15px; font-size: 18px; color: #fff; background-color: #007bff; text-decoration: none; border-radius: 5px;">Renew Your Link</a>
-            <p style="color: #999; font-size: 14px; margin-top: 20px;">If you have any questions, feel free to contact us.</p>
+                <div style="background-color: #0e0d21; color: white; font-family: 'Jost', sans-serif; padding: 20px; border-radius: 10px;">
+                    <link rel="preconnect" href="https://fonts.googleapis.com">
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@100..900&display=swap" rel="stylesheet">
+                    <div style="text-align: center; padding-bottom: 20px;">
+                <h1 style="margin: 0; color: white; font-style: italic;">OmniMize</h1>
+            </div>
+            <div style="font-family: 'Jost', sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #010015; text-align: center; border-radius: 10px;">
+                <h1 style="color: white; font-weight: 500;">Ihr persönlicher Link läuft bald ab!</h1>
+                <p style="color: white; font-size: 16px;">Hallo ${customerName},</p>
+                <p style="color: white; font-size: 16px;">Ihr persönlicher Download-Link läuft am <strong>${expirationDate}</strong> ab. Stellen Sie sicher, dass Sie ihn rechtzeitig erneuern!</p>
+                <a href="${customerLink}" style="display: inline-block; padding: 12px 20px; margin-top: 15px; font-size: 20px; color: #fff; background: linear-gradient(91deg, #5b9ace 0%, #264aa7 100%); text-decoration: none; border-radius: 5px;">Link erneuern</a>
+                <p style="color: #999; font-size: 14px; margin-top: 20px;">Wenn Sie Fragen haben, können Sie uns gerne kontaktieren.</p>
+            </div>
         </div>
         `;
             }
             case NotificationMessageType.NEW_ACCESS_LINK: {
                 return `
-        <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f4f4f4; text-align: center;">
-            <h1 style="color: #333;">Your new access link</h1>
-            <p style="color: #555; font-size: 16px;">Hello ${customerName},</p>
-            <p style="color: #555; font-size: 16px;">Here is your new access link. Click the button below to start using it:</p>
-            <a href="${customerLink}" style="display: inline-block; padding: 12px 20px; margin-top: 15px; font-size: 18px; color: #fff; background-color: #007bff; text-decoration: none; border-radius: 5px;">Access Now</a>
-            <p style="color: #999; font-size: 14px; margin-top: 20px;">If you have any questions, feel free to contact us.</p>
+            <div style="background-color: #0e0d21; color: white; font-family: 'Jost', sans-serif; padding: 20px; border-radius: 10px;">
+                <link rel="preconnect" href="https://fonts.googleapis.com">
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                <link href="https://fonts.googleapis.com/css2?family=Jost:wght@100..900&display=swap" rel="stylesheet">
+                <div style="text-align: center; padding-bottom: 20px;">
+            <h1 style="margin: 0; color: white; font-style: italic;">OmniMize</h1>
+        </div>
+        <div style="font-family: 'Jost', sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #010015; text-align: center; border-radius: 10px;">
+            <h1 style="color: white; font-weight: 500;">Ihr neuer Zugang ist bereit!</h1>
+            <p style="color: white; font-size: 16px;">Hallo ${customerName},</p>
+            <p style="color: white; font-size: 16px;">Hier ist Ihr neuer Zugang. Klicken Sie auf den Button unten, um ihn zu nutzen:</p>
+            <a href="${customerLink}" style="display: inline-block; padding: 12px 20px; margin-top: 15px; font-size: 20px; color: #fff; background: linear-gradient(91deg, #5b9ace 0%, #264aa7 100%); text-decoration: none; border-radius: 5px;">Jetzt zugreifen</a>
+            <p style="color: #999; font-size: 14px; margin-top: 20px;">Wenn Sie Fragen haben, können Sie uns gerne kontaktieren.</p>
+        </div>
         </div>
         `;
             }
